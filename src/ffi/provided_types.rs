af64fac314066eef2dfa32460d0759789cfffc63
Maxim Kozlov <sebekerka@gmail.com>
2023-08-29 11:33:37 +0600

    documented types

--- src/ffi/provided_types.rs
+++ src/ffi/provided_types.rs
@@ -1,30 +1,42 @@
+use chrono::{Datelike, Offset, Timelike};
 use std::{
     ffi::c_int,
     ptr,
     slice::{from_raw_parts, from_raw_parts_mut},
 };
 
-use chrono::{Datelike, Offset, Timelike};
-
 use super::memory_manager::MemoryManager;
 
+/// Type representing 1C date and time values
+/// # Fields
+/// * `sec` - seconds after the minute - [0, 60] including leap second
+/// * `min` - minutes after the hour - [0, 59]
+/// * `hour` - hours since midnight - [0, 23]
+/// * `mday` - day of the month - [1, 31]
+/// * `mon` - month of the year - [0, 11]
+/// * `year` - years since 1900
+/// * `wday` - days since Sunday - [0, 6]
+/// * `yday` - days since January 1 - [0, 365]
+/// * `isdst` - daylight savings time flag
+/// * `gmtoff` - seconds east of UTC (unix only)
+/// * `zone` - timezone abbreviation (unix only)
 #[repr(C)]
 #[derive(Clone, Copy, Default)]
 pub struct Tm {
-    pub sec: c_int, // seconds after the minute - [0, 60] including leap second
-    pub min: c_int, // minutes after the hour - [0, 59]
-    pub hour: c_int, // hours since midnight - [0, 23]
-    pub mday: c_int, // day of the month - [1, 31]
-    pub mon: c_int, // months since January - [0, 11]
-    pub year: c_int, // years since 1900
-    pub wday: c_int, // days since Sunday - [0, 6]
-    pub yday: c_int, // days since January 1 - [0, 365]
-    pub isdst: c_int, // daylight savings time flag
+    pub sec: c_int,
+    pub min: c_int,
+    pub hour: c_int,
+    pub mday: c_int,
+    pub mon: c_int,
+    pub year: c_int,
+    pub wday: c_int,
+    pub yday: c_int,
+    pub isdst: c_int,
 
     #[cfg(target_family = "unix")]
-    pub gmtoff: std::ffi::c_long, // seconds east of UTC
+    pub gmtoff: std::ffi::c_long,
     #[cfg(target_family = "unix")]
-    pub zone: std::ffi::c_char, // timezone abbreviation
+    pub zone: std::ffi::c_char,
 }
 
 impl From<chrono::DateTime<chrono::FixedOffset>> for Tm {
@@ -109,6 +121,11 @@ impl From<Tm> for chrono::DateTime<chrono::FixedOffset> {
     }
 }
 
+/// Type representing 1C variant values
+/// # Fields
+/// `mem` - pointer to the MemoryManager object
+/// `variant` - pointer to the TVariant object
+/// `result` - pointer to the result of the operation
 pub struct ReturnValue<'a> {
     pub mem: &'a MemoryManager,
     pub variant: &'a mut TVariant,
@@ -117,29 +134,36 @@ pub struct ReturnValue<'a> {
 
 #[allow(dead_code)]
 impl<'a> ReturnValue<'a> {
+    /// Creates a new ReturnValue object
     pub fn set_empty(self) {
         self.variant.vt = VariantType::Empty;
     }
+
+    /// Sets the value of the ReturnValue object to integer `i32`
     pub fn set_i32(self, val: i32) {
         self.variant.vt = VariantType::Int32;
         self.variant.value.i32 = val;
     }
 
+    /// Sets the value of the ReturnValue object to bool `bool`
     pub fn set_bool(self, val: bool) {
         self.variant.vt = VariantType::Bool;
         self.variant.value.bool = val;
     }
 
+    /// Sets the value of the ReturnValue object to float `f64`
     pub fn set_f64(self, val: f64) {
         self.variant.vt = VariantType::Double;
         self.variant.value.f64 = val;
     }
 
+    /// Sets the value of the ReturnValue object to date-time `Tm`
     pub fn set_date(self, val: Tm) {
         self.variant.vt = VariantType::Time;
         self.variant.value.tm = val;
     }
 
+    /// Sets the value of the ReturnValue object to UTF-16 `&[u16]`
     pub fn set_str(self, val: &[u16]) {
         let Some(ptr) = self.mem.alloc_str(val.len()) else {
             *self.result = false;
@@ -155,6 +179,7 @@ impl<'a> ReturnValue<'a> {
         self.variant.value.data_str.len = val.len() as u32;
     }
 
+    /// Sets the value of the ReturnValue object to blob `&[u8]`
     pub fn set_blob(self, val: &[u8]) {
         let Some(ptr) = self.mem.alloc_blob(val.len()) else {
             *self.result = false;
@@ -169,45 +194,23 @@ impl<'a> ReturnValue<'a> {
         self.variant.value.data_blob.ptr = ptr.as_ptr();
         self.variant.value.data_blob.len = val.len() as u32;
     }
-
-    pub fn alloc_str(self, len: usize) -> Option<&'a mut [u16]> {
-        let Some(ptr) = self.mem.alloc_str(len) else {
-            *self.result = false;
-            return None;
-        };
-
-        unsafe { ptr::write_bytes(ptr.as_ptr(), 0, len) };
-
-        self.variant.vt = VariantType::WStr;
-        self.variant.value.data_str.ptr = ptr.as_ptr();
-        self.variant.value.data_str.len = len as u32;
-
-        Some(unsafe { from_raw_parts_mut(ptr.as_ptr(), len) })
-    }
-
-    pub fn alloc_blob(self, len: usize) -> Option<&'a mut [u8]> {
-        let Some(ptr) = self.mem.alloc_blob(len) else {
-            *self.result = false;
-            return None;
-        };
-
-        unsafe { ptr::write_bytes(ptr.as_ptr(), 0, len) };
-
-        self.variant.vt = VariantType::Blob;
-        self.variant.value.data_blob.ptr = ptr.as_ptr();
-        self.variant.value.data_blob.len = len as u32;
-
-        Some(unsafe { from_raw_parts_mut(ptr.as_ptr(), len) })
-    }
 }
 
+/// Represents 1C variant values for parameters
 pub enum ParamValue {
+    /// Empty value
     Empty,
+    /// Boolean value
     Bool(bool),
+    /// Integer value
     I32(i32),
+    /// Float value
     F64(f64),
+    /// Date-time value
     Date(Tm),
+    /// UTF-16 string value
     Str(Vec<u16>),
+    /// Blob value
     Blob(Vec<u8>),
 }
 
@@ -272,6 +275,10 @@ enum VariantType {
     Undefined = 0xFFFF,
 }
 
+/// Type representing stored String data
+/// # Fields
+/// * `ptr` - pointer to the data
+/// * `len` - length of the data
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct DataStr {
@@ -279,6 +286,10 @@ struct DataStr {
     pub len: u32,
 }
 
+/// Type representing stored Blob data
+/// # Fields
+/// * `ptr` - pointer to the data
+/// * `len` - length of the data
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct DataBlob {
@@ -286,6 +297,14 @@ struct DataBlob {
     pub len: u32,
 }
 
+/// Type encapsulating 1C variant values
+/// # Fields
+/// * `bool` - boolean value
+/// * `i32` - integer value
+/// * `f64` - float value
+/// * `tm` - date-time value
+/// * `data_str` - UTF-16 string value
+/// * `data_blob` - blob value
 #[repr(C)]
 union VariantValue {
     pub bool: bool,
@@ -296,6 +315,7 @@ union VariantValue {
     pub data_blob: DataBlob,
 }
 
+/// Type encapsulating 1C variant values for internal use
 #[repr(C)]
 pub struct TVariant {
     value: VariantValue,
