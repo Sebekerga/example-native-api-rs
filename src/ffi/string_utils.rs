b6965e24f4a7b603619f246a932fb2d218eb2fe4
Maxim Kozlov <sebekerka@gmail.com>
2023-08-29 11:03:43 +0600

    documented string utils

--- src/ffi/string_utils.rs
+++ src/ffi/string_utils.rs
@@ -1,5 +1,10 @@
 use std::slice::from_raw_parts;
 
+/// Helper function to convert pointer to UTF-16 string to Rust slice
+/// # Arguments
+/// * `s` - pointer to UTF-16 string
+/// # Returns
+/// `&[u16]` - slice of UTF-16 characters
 pub unsafe fn get_str<'a>(s: *const u16) -> &'a [u16] {
     unsafe fn strlen(s: *const u16) -> usize {
         let mut i = 0;
@@ -13,11 +18,21 @@ pub unsafe fn get_str<'a>(s: *const u16) -> &'a [u16] {
     from_raw_parts(s, len)
 }
 
+/// Helper function to convert Rust string to UTF-16 string
+/// # Arguments
+/// * `s` - Rust string
+/// # Returns
+/// `Vec<u16>` - UTF-16 string without null terminator
 #[cfg(target_family = "unix")]
 pub fn os_string_nil(s: &str) -> Vec<u16> {
     s.encode_utf16().collect()
 }
 
+/// Helper function to convert Rust string to UTF-16 string
+/// # Arguments
+/// * `s` - Rust string
+/// # Returns
+/// `Vec<u16>` - UTF-16 string with null terminator
 #[cfg(target_family = "windows")]
 pub fn os_string_nil(s: &str) -> Vec<u16> {
     let os_str = std::ffi::OsStr::new(s);
@@ -26,6 +41,11 @@ pub fn os_string_nil(s: &str) -> Vec<u16> {
         .collect()
 }
 
+/// Helper function to convert UTF-16 string to Rust string
+/// # Arguments
+/// * `s` - UTF-16 string
+/// # Returns
+/// `String` - Rust string
 pub fn from_os_string(s: &[u16]) -> String {
     String::from_utf16_lossy(s)
         .trim_end_matches(char::from(0))
